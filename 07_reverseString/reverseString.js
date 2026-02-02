724e945af82af62952be478a4597fe12ce066443
Gregory <gregorytorrico@gmail.com>
2026-02-03 00:30:01 +0100

    Assignment Completed
    We turned the string into an array to use the respective methods.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("")
 };
+//first we transform our string into an array using .split("") 
+//it will separate the string into substrings e.g. (h,e,l,l,o)
+//now that we have an array we use .reverse()
+//then use .join("") 
 
 // Do not edit below this line
 module.exports = reverseString;
