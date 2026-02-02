e560be41be1b86101d5e95f1e74b9301474a4c37
Gregory <gregorytorrico@gmail.com>
2026-02-02 23:46:09 +0100

    Assignment Completed
    Line 2 was added to proof the code.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+    if (num < 0) return "ERROR";
+    let string = "";
+    for (let i=0; i < num; i++) {
+          string += word;
+    }
+    return string;
 };
+//first we error proof on line 2 
+//we declare our string variable making it empty ""
+//we create our for loop on line 4
+//we concatenate our words into our string on line 5 (string = string + word)
 
 // Do not edit below this line
 module.exports = repeatString;
