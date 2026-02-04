0ade5ae37bcc8f4c5c80700af1ffa4ffdb6bafe6
Gregory <gregorytorrico@gmail.com>
2026-02-04 21:56:54 +0100

    Assignment Completed
    Rest parameter was used to store multiple arguments

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter((item) => !args.includes(item));
 };
-
+//we used rest parameters (it allows to input multiple arguments in the form of an array)
+//second line .filter is combined with ! (NOT Operator) so it will include in array the values that are NOT in args
+//without ! it will include in array the items in args
 // Do not edit below this line
 module.exports = removeFromArray;
